7a74d0c9a422b6d0084ff82eedd0c1b2eede7fcc
Jason Donnelly <41059325+jasondonnellya@users.noreply.github.com>
2022-05-03 16:22:07 +0100

    Update Content.vue

--- components/Veriteer/Content.vue
+++ components/Veriteer/Content.vue
@@ -9,6 +9,9 @@ export default {
     },
     async mounted() {
         if(!this.content) return
+        /**
+         * TODO: replace Toast with https://www.contentful.com/developers/docs/javascript/tutorials/rendering-contentful-rich-text-with-javascript/
+         */
         const Viewer = (await import('@toast-ui/editor/dist/toastui-editor-viewer')).default;
         const viewer = new Viewer({
             el: document.querySelector('#viewer'),
