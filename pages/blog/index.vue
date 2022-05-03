0d758f4d0a9022fcd5bef115144f337fd17e99f9
Jason Donnelly <41059325+jasondonnellya@users.noreply.github.com>
2022-05-03 09:47:34 +0100

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { getBlogs } from '@/plugins/contentful.js'
-const { data: blogs } = useAsyncData('blogs', () => getBlogs())
+const { data: blogs } = useAsyncData('blogs', () => getBlogs()) // ? remove :blogs
 
 /**
  * ! Either $fetch is required for reactive asynchronous requests or script setup is not fired on route change.
@@ -11,9 +11,11 @@ const { data: blogs } = useAsyncData('blogs', () => getBlogs())
  * 
  * ? Contentful URL returns Blob JS type, not sure if this is because of nuxt $fetch or Contentful headers.
  * ? Blob seems empty but also cannot be stringified by JSON.stringify().
- * ? useAsyncData must stringify data as it throws warning and nothing happens. (Cannot stringify arbitrary non-POJOs Blob)
+ * ? useAsyncData must use stringify data as it throws warning and nothing happens. (Cannot stringify arbitrary non-POJOs Blob)
  * ? FileReader is not available in NodeJS, only client-side JavaScript so can't convert Blob to JSON.
+ * ? FileReader may not be necessary, try Blob.text() but Blob still returns empty. // https://developer.mozilla.org/en-US/docs/Web/API/Blob
  */
+
 /*
 import { createClient } from 'contentful'
 const { cdaUri, spaceId, environmentId, cdaToken } = useRuntimeConfig()
@@ -23,7 +25,7 @@ const client = createClient({
         space: spaceId,
         accessToken: cdaToken
     });
-const url = `${cdaUri}/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${cdaToken}&content_type=blogPost`
+const url = `${cdaUri}/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${cdaToken}&content_type=blogPost` // ? remove &content_type=blogPost
 const blogData = await $fetch(url)
 const { data: blogs } = useAsyncData('blogs', () => blogData)
 */
